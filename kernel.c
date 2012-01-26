4a8e8404042989e3a5e0ee9829c9689d8cae974b
Sarantis <skyr@sarantis-UbuntuPC.(none)>
2012-01-27 00:20:13 +0200

    commit for saving and testing scheduler almost working

--- kernel.c
+++ kernel.c
@@ -27,10 +27,17 @@ typedef struct PCB_s {
 	enum ProcState state;
 }PCB;
 
-PCB* ProcessTable[MAX_PROC];
+typedef struct rlist {
+	PCB* proc;
+	struct rlist* prev;
+	struct rlist* next;
+}Read;
+
+Read *head,*tail,*curproc;
+PCB ProcessTable[MAX_PROC];
 Pid_t PCBcnt;
-PCB* curproc;
-Mutex kernel_lock;
+Mutex kernel_lock = MUTEX_INIT;
+ucontext_t kernel_context;
 
 /*
  *
@@ -48,6 +55,7 @@ quantum_itimer.it_value.tv_sec = 0L;
 quantum_itimer..it_value.tv_usec = QUANTUM;
 */
 
+
 void reset_timer()
 {
 	setitimer(ITIMER_VIRTUAL, &quantum_itimer, NULL);
@@ -57,26 +65,47 @@ sigset_t scheduler_sigmask;
 /*sigemptyset(&scheduler_sigmask);
 sigaddset(&scheduler_sigmask, SIGVTALRM);*/
 
+void pause_scheduling() {sigprocmask(SIG_BLOCK,&scheduler_sigmask, NULL);}
+void resume_scheduling() {sigprocmask(SIG_UNBLOCK, &scheduler_sigmask, NULL);}
+
 void schedule(int sig){
 	PCB *old, *new;
-
+	Read* i;
 	//pause scheduling
 	pause_scheduling();
-	old = curproc;
-	//new = pick_next_thread();
-
+	old = curproc->proc;
+	new = head->proc;
+	head = head->next;
+	//printf("\nArg %d",i->proc->pid);
+	//printf("\nold %d new %d curproc %d",old->pid,new->pid,curproc->proc->pid);
 	if(old->state==FINISHED);
 		//final_cleanup(old);
 
 	reset_timer();
 	//resume scheduling
 	resume_scheduling();
-	curproc = new;
+	curproc->proc = new;
+	//printf("\nold %d new %d curproc %d",old->pid,new->pid,curproc->pid);
 	if(old!=new)
-		swapcontext(&new->context,&old->context);
+		swapcontext(&(old->context),&(new->context));
+}
+
+void wakeup(Pid_t pid){
+	ProcessTable[pid].state = READY;
 }
 
 void yield() {schedule(0);}
+
+void release_and_sleep(Mutex* cv){
+	//printf("\ncurp %d %d %d \n",curproc->proc->pid,head->proc->pid,tail->proc->pid);
+	Mutex_Lock(cv);
+	//printf("\ncurp %d %d %d \n",curproc->proc->pid,head->proc->pid,tail->proc->pid);
+	curproc->proc->state = SLEEPING;
+	//printf("\ncurp %d %d %d \n",curproc->proc->pid,head->proc->pid,tail->proc->pid);
+	Mutex_Unlock(cv);
+	yield();
+}
+
 
 void run_scheduler()
 {
@@ -91,8 +120,8 @@ void run_scheduler()
 
 	reset_timer();
 
-	curproc = ProcessTable[0];
-	swapcontext(&_saved_context, &(curproc->context));
+	curproc = head->prev;
+	swapcontext(&kernel_context,&curproc->proc->context);
 }
 
 int Mutex_TryLock(Mutex *lock)
@@ -138,7 +167,8 @@ void Cond_Wait(Mutex* mutex, CondVar* cv)
 	cv->wstail = &(waitnode.next);
 
 	Mutex_Unlock(mutex);
-	//release_and_sleep(STOP_CVWAIT, &condvar_mutex);
+	printf("\nold  new \n");
+	release_and_sleep(mutex);
 
 	Mutex_Lock(mutex);
 }
@@ -148,7 +178,7 @@ static void doSignal(CondVar* cv)
 	if(cv->waitset != NULL){
 		tinyos_cv_waiter *node = cv->waitset;
 		cv->waitset = node->next;
-		//wakeup(node->pid,STOP_CVWAIT);
+		wakeup(node->pid);
 	}
 }
 
@@ -167,21 +197,18 @@ void Cond_Broadcast(CondVar* cv)
 	Mutex_Unlock(&condvar_mutex);
 }
 
-void init_context(ucontext_t* uc, void* stack, size_t stack_size, Task task, int argl, void* args)
+void init_context(ucontext_t* uc, void* stack, size_t stack_size, Task call, int argl, void* args)
 {
 	getcontext(uc);
 	uc->uc_link = NULL;
 	uc->uc_stack.ss_sp = stack;
 	uc->uc_stack.ss_size = stack_size;
 	uc->uc_stack.ss_flags = 0;
-	makecontext(uc, task, 2, argl, args);
+	makecontext(uc, call, 2, argl, args);
 }
 
 #define PROCESS_STACK_SIZE 65536
 
-
-void pause_scheduling() {sigprocmask(SIG_BLOCK,&scheduler_sigmask, NULL);}
-void resume_scheduling() {sigprocmask(SIG_UNBLOCK, &scheduler_sigmask, NULL);}
 
 /*
  *
@@ -197,33 +224,50 @@ void Exit(int exitval)
 
 Pid_t Exec(Task call, int argl, void* args)
 {
-	ucontext_t unew;//,uold;
+	ucontext_t unew;
 	void* stack = malloc(PROCESS_STACK_SIZE);
-	init_context(&unew, stack, PROCESS_STACK_SIZE, call, 2, 0);
-	PCB* temp = malloc(sizeof(PCB));
-	ProcessTable[PCBcnt] = temp;
-	(ProcessTable[PCBcnt])->context = unew;
-	(ProcessTable[PCBcnt])->parent_pid = (PCBcnt==0) ? 0 : curproc->pid;
-	(ProcessTable[PCBcnt])->pid = PCBcnt;
-	(ProcessTable[PCBcnt])->state = READY;
-	curproc = ProcessTable[PCBcnt++];
-	call(argl,args);
-	printf("\n\n%d\n\n",curproc->pid);
-	return curproc->pid;
+	PCBcnt = PCBcnt+1;
+	init_context(&unew, stack, PROCESS_STACK_SIZE, call, argl, args);
+	ProcessTable[PCBcnt].context = unew;
+	ProcessTable[PCBcnt].parent_pid = (curproc==NULL) ? 0 : curproc->proc->pid;
+	ProcessTable[PCBcnt].pid = PCBcnt;
+	ProcessTable[PCBcnt].state = READY;
+	Read *temp = malloc(sizeof(Read*));
+	temp->proc = &ProcessTable[PCBcnt];
+	temp->next = NULL;
+	temp->prev = NULL;
+	if(head==NULL)
+	{
+		head = temp;
+		head->next = head;
+		head->prev = head;
+	}
+	else
+	{
+		temp->prev = head->prev;
+		temp->prev->next = temp;
+		head->prev = temp;
+		temp->next = head;
+	}
+	return 1;//curproc->pid;
 }
 
 Pid_t GetPid()
 {
-  return curproc->pid;
+  return curproc->proc->pid;
 }
 
 
 Pid_t WaitChild(Pid_t cpid, int* status)
 {
-
-	if((ProcessTable[--PCBcnt])->state == FINISHED)
-		return ProcessTable[PCBcnt]->pid;
+	while(1)
+		release_and_sleep(&condvar_mutex);
 	return NOPROC;
+	/*printf("Mpike!");
+	if(ProcessTable[1].state == FINISHED)
+		return ProcessTable[PCBcnt].pid;
+	printf("Mpike!");
+	return NOPROC;*/
 }
 
 /*
@@ -235,12 +279,17 @@ Pid_t WaitChild(Pid_t cpid, int* status)
 void boot(Task boot_task, int argl, void* args)
 {
 	PCBcnt = 0;
-	Exec(boot_task,argl,args);
+	curproc=NULL;
+	head = NULL;
+	tail = head;
 	sigemptyset(&scheduler_sigmask);
 	sigaddset(&scheduler_sigmask, SIGVTALRM);
 	quantum_itimer.it_interval.tv_sec = 0L;
 	quantum_itimer.it_interval.tv_usec = QUANTUM;
 	quantum_itimer.it_value.tv_sec = 0L;
 	quantum_itimer.it_value.tv_usec = QUANTUM;
+	Exec(boot_task,argl,args);
+	run_scheduler();
+	printf("eftase");
 }
 
